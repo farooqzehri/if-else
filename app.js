8c327c722fbdedd268b1e935802351bb51d0fba5
farooqzehri <farooqahmedbaloch17@gmail.com>
2025-09-30 19:30:03 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,147 @@
+// let age = prompt("Enter your age:");
+// age => 17(age);
+// if (age > 17) {
+//   alert("You are an adult.");
+//     } else {
+//   alert("You are a minor.");
+//  }
+  // Ask the user for a number
+//   let number = prompt("Enter a number:");
+
+//   // Convert the input to a number
+//   number = Number(number);
+
+  // Check if the number is even or odd
+//   if (number % 2 === 0) {
+//     alert("Even number");
+//   } else {
+//     alert("Odd number");
+//   }
+  // var clase = 7
+  // if (clase <= 2){console.log("your child ");
+  //   }else {console.log("this is not your class");}
+   
+
+// var Admin = farooq 
+    // if (Admin => farooq)prompt("welcome")
+    // else
+    //      prompt("you are not allowed")
+  
+    // var age = prompt("your age ")
+    // if (age <= 17)alert("ne daska chuna se us ")
+    //   else alert("ganj karos")
+    //         WORKING
+
+    // var battery = 144
+    // if (battery <= 20){console.log("low charge")}
+    // if (battery < 50 > 79){console.log("normal charging")}
+    // if (battery >= 80 ){console.log("good charging")}
+    // if (battery >= 100)(console.log("full"))
+
+
+
+// 2. Ask the user to enter a number. If it's positive, log: "Positive number"; else log:
+// "Not positive".
+// var number = prompt("enter a number")
+// number = number * 1
+// if (number > 0){console.log(("positive number"));
+// }
+// else {console.log("not positive");
+// }
+
+
+// 3. Ask the user for a number. If it’s even, alert: "Even number"; else alert: "Odd
+// number".
+//  var number = prompt("enter a number")
+//  number = number * 1
+//     if (number % 2 == 0){alert("odd number");}if (number % 2 != 0){alert("even number");}
+
+
+// 4. Ask for a username. If the username is "admin", display "Welcome, admin!"; else
+// display "Access denied."
+// var username = prompt("enter username")
+// if (username == "admin")alert("welcome admin")
+// else {alert("access denied")}
+
+
+// 5. Ask for two numbers. If they are equal, log "Both numbers are the same";
+// otherwise, log "The numbers are different".
+// var number1 = prompt ("enter first number")
+// var number2 = prompt ("enter second number")
+// if (number1 == number2)console.log("both numbers are the same");
+// else console.log("the numbers are different");
+
+
+// 6. Ask the user to enter two numbers. Check which one is larger and alert: "First is
+// larger" or "Second is larger".
+//   var number1 = prompt ("enter first number")
+//  var number2 = prompt ("enter second number")
+//   if (number1 > number2)alert("first is larger");  
+//     else alert("second is larger");
+
+
+    //  Ask the user to enter a temperature. If it’s over 30, alert "It's hot!"; else alert "It's
+// not hot."
+// var temperature = prompt("enter temperatue")
+// if (temperature >= 30)alert("it's hot");else{alert("it's not hot")};
+// var name = prompt("what is your name")
+// if (name == "alice")alert("welcome: -" + name ); else{alert("you are not alice")}
+// var password = prompt("enter your password")
+// if (password == "sangar12")alert("correct password");else{alert("incorrect password")};
+// var time = prompt("what is the time")
+// if (time <= 11)alert("good morning"); else{alert("good afternoon")}
+
+
+// 15. Ask the user to enter a number. If it's divisible by 5, log "Divisible by 5"; else log
+// "Not divisible by 5".
+//  var number = prompt("enter number")
+//  if (number % 5 == 0)console.log("divisable by 5");else{console.log("not divisable by 5")};
+// var game = prompt("wanna play a game ")
+// if (game == "yes")alert("let's play a game");
+// else{alert("maybe next time")}
+
+
+// var color = prompt("what is your favorite color")
+// if (color == "blue")alert("cool choice");else{alert("intresting color")}
+// var day = prompt("what is the day ")
+// if (day == "sunday")alert("yeah weekend:!"); else{alert("weekday")}
+
+
+// var fruits = prompt("enter a fruit")
+// if (fruits == "apple")alert("we have that!");
+// else if (fruits == "banana")alert("we have that:!");
+// else{alert("sorry out of stock:(")}
+//                 working with if else if else
+
+
+//     var game = prompt("type start to begin")
+// if (game == "start")alert("game started");
+// else{alert("input not working")};
+
+
+// var marks = prompt("enter your marks ")
+// if (marks >= 80 && marks <= 100)alert("Distinction");
+// if (marks >= 50 && marks <= 79)alert("good marks");
+// if (marks <= 50)alert("fail-");
+// if (marks <= 0 )alert("invalid marks");
+// if (marks >= 101 ) alert("invalid marks");
+//               working clearly
+
+
+// var age = prompt("what is your age ")
+// var card = prompt("du you have a card")
+// if (age >= 60 && card == "yes")alert("seneir and membership discount");
+// else if (age >= 60 )alert("senior discount");
+// else if (card == "123")alert("premuim discount");
+// else(alert("no discount"))
+//                      working clearly
+
+
+//                     
+// var age = prompt("what is your age ")
+// var country = prompt("where do you live")
+// var visa = prompt("visa is valid")
+// if (age >= 17 && country == "pakistan" && visa == "yes")alert("welcome");
+// else if (age <= 35 && country == "india" && visa == "yes")alert("welcome");
+// else if (age <= 16 )alert(" no entry");else{alert("no entry")}
+//                just working but not the i like
